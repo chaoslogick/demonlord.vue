9ca3f9cfb79421011019915c770d58062399ff4d
Michael Perry Goodman <mpg@demonlord.org>
2021-12-21 23:31:55 -0600

    test adding new nav item for deployment

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,5 +1,6 @@
 <template>
   <ul class="main-menu">
     <li><router-link to="/resume">/resume</router-link></li>
+    <li><router-link to="/about">/about</router-link></li>
   </ul> <!-- /nav -->
 </template>
