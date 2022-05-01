22252fb943998c23604b4b990ff7a7d5f7524464
Michael Perry Goodman <mpg@demonlord.org>
2022-05-01 17:28:49 -0500

    Add a bunch of gear nerd stuff to music page

--- src/components/PageMusic.vue
+++ src/components/PageMusic.vue
@@ -38,6 +38,86 @@
           <a href="https://voidenvogue.bandcamp.com/album/varia-ep">Varia EP by Void En Vogue</a>
         </iframe>
       </div>
+
+<div class="sidebar">
+        <span class="eyebrow">Hardware & Synths</span>
+        <ul>
+          <li>Yamaha MT4X 4-Track</li>
+          <li>Antelope Audio ZenTour</li>
+          <li>Genelec 8010A + Sub</li>
+          <li>Beyerdynamic DT990 Pro</li>
+          <li>Grado SR80e</li>
+          <li>&nbsp;</li>
+          <li>Kurzweil K2VX</li>
+          <li>Access Virus C</li>
+          <li>Nord Lead Rack 1</li>
+          <li>Novation K-Station</li>
+          <li>Behringer DeepMind 12</li>
+          <li>Roli Seaboard BLOCK</li>
+          <li>&nbsp;</li>
+          <li>Joemeek VC3Q Preamp</li>
+          <li>TC Electronic Fireworx</li>
+          <li>dbx 166XL Stereo Comp</li>
+          <li>Some other stuff...</li>
+        </ul>
+
+        <span class="eyebrow">Eurorack Modular</span>
+        <ul>
+          <li>Later...</li>
+        </ul>
+
+        <span class="eyebrow">Guitar Gear</span>
+        <ul>
+          <li>Kemper Profiler Rack</li>
+          <li>Marshall JCM900 4100</li>
+          <li>Traynor TS-25</li>
+          <li>&nbsp;</li>
+          <li>Ibanez RGA42HPSF</li>
+          <li>Ibanez RG421RW</li>
+          <li>Jackson Performer PS3T</li>
+          <li>Jackson Warrior JS32W</li>
+          <li>Gibson Les Paul Special</li>
+          <li>Kaman/Ovation GTX 33</li>
+          <li>Kaman/Ovation GTX 38</li>
+          <li>&nbsp;</li>
+          <li>ProCo RAT</li>
+          <li>Dwarfcraft Necromancer</li>
+          <li>Maxon OD-808</li>
+          <li>MXR/CAE MC402 Boost</li>
+          <li>MXR DynaComp</li>
+          <li>Boss DS-1</li>
+          <li>Boss DS-2</li>
+          <li>Boss HM-3</li>
+          <li>Line 6 DL4</li>
+        </ul>
+
+        <span class="eyebrow">DAWs, SoftSynths, Plugins</span>
+        <ul>
+          <li>FL Studio</li>
+          <li>Bitwig Studio</li>
+          <li>Logic Pro X</li>
+          <li>Adobe Audition</li>
+          <li>Amadeus Pro</li>
+          <li>&nbsp;</li>
+          <li>NI Reaktor 6</li>
+          <li>Max 8</li>
+          <li>PureData</li>
+          <li>Supercollider</li>
+          <li>&nbsp;</li>
+          <li>Spectrasonics Omnisphere</li>
+          <li>Synapse Audio DUNE</li>
+          <li>Rob Papen Predator</li>
+          <li>DiscoDSP Discovery Pro</li>
+          <li>Novation V-Station</li>
+          <li>&nbsp;</li>
+          <li>FabFilter Suite</li>
+          <li>Eventide Suite</li>
+          <li>ValhallaDSP Plugins</li>
+          <li>Michael Norris Plugins</li>
+          <li>GRM Tools</li>
+          <li>A few secret weapons...</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
