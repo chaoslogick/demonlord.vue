9893662cbcd94e945f12921ed79d067c116cb6fa
Michael Perry Goodman <mpg@demonlord.org>
2022-05-16 08:15:36 -0500

    update text resume, update nav

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="main-menu">
-    <li><router-link to="/music">/music</router-link></li>
     <li><router-link to="/resume">/resume</router-link></li>
+    <li><router-link to="/music">/music</router-link></li>
   </ul> <!-- /nav -->
 </template>
