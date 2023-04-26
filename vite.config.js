fe286ab5aeeb640dd72f221c30eb2d6057c58e86
Michael Perry Goodman <michaelperrygoodman@gmail.com>
2023-04-26 10:34:15 -0500

    vite migration cont

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
+// reconfigure to compile scss, use .env
+
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
+  },
+  ({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
   }
-})
+)
